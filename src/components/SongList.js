003687948d91e93e80fe0631a6fb4f568e46d4ab
Aresky Berkane <areskyberkane@gmail.com>
2020-01-28 15:43:11 +0000

    finish mvp

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,27 @@
 import React, { Component } from 'react';
+import SongDetail from './SongDetail';
 
+const SongList = ({songs}) => {
+
+    // functions:*****************
+    const songNodes = songs.map( song => {
+        return(
+            <SongDetail 
+            artist={song["im:artist"].label}
+            title={song.title.label}
+            imageUrl={song["im:image"][2].label}
+            ></SongDetail>
+        )
+    })
+
+
+    // *****************
+
+    return(
+        <ol>    
+        {songNodes}
+        </ol>
+    )
+}
 
 export default SongList;
