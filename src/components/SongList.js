d7e7fd8131112aba60f2b5bccad29ebc83244a61
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-28 20:57:00 +0000

    CSS practice

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import SongDetail from './SongDetail';
+import './SongList.css'
 
 const SongList = ({songs}) => {
 
     // functions:*****************
     const songNodes = songs.map( song => {
         return(
-            <SongDetail 
+            <SongDetail
             // artist={song["im:artist"].label}
             title={song.title.label}
             imageUrl={song["im:image"][2].label}
             audioUrl={song.link[1].attributes.href}
+            key={song.id.attributes["im:id"]}
             ></SongDetail>
         )
     })
@@ -19,7 +21,7 @@ const SongList = ({songs}) => {
     // *****************
 
     return(
-        <ol className="ordered-list">    
+        <ol className="ordered-list">
         {songNodes}
         </ol>
     )
--- src/containers/ChartBox.js
+++ src/containers/ChartBox.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SongList from '../components/SongList';
+import './ChartBox.css'
 
 class ChartBox extends Component{
 
@@ -10,29 +11,31 @@ class ChartBox extends Component{
         }
 
     }
-        
+
         // functions **********************
         componentDidMount(){
             const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json";
-            
+
             fetch(url)
             .then(res => res.json())
             .then(feed => this.setState({songs: feed.feed.entry}))
             .catch(err => console.log(err)
             )
             console.log(this.songs);
-            
+
         }
-        
+
 
 
 
     // **********************
-    
+
 
     render(){
         return(
+          <div className="chart-box">
             <SongList songs = {this.state.songs}></SongList>
+          </div>
         )
     }
 }
